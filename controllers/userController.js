187c5a268ef0f5af0fee90c826657899196b082f
Mossaiko1 <alejovillada1314@gmail.com>
2024-09-02 18:09:19 -0500

    ApiRest con el CRUD de user terminado + login JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,89 @@
-import User from '../models/user.js'
-import bcrypt from 'bcryptjs'
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 /*
 {
-    "email":"dalz@sena.edu.co",
-    "password":1234
+    "email": "dalz@sena.edu.co",
+    "password": "1234"
 }
-    */
-export async function createUser(req, res){
-    const body = req.body
+*/
+
+export async function createUser(req, res) {
+    const body = req.body;
     try {
-        const user = new User(body)
-        user.password = await bcrypt.hash(body.password, 10)//Manipulando el objeto
-        await user.save()
-        res.status(201).json(user)
+        const user = new User(body);
+        user.password = await bcrypt.hash(body.password, 10);
+        await user.save();
+        res.status(201).json(user);
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json({ message: 'Server error', error });
     }
 }
-//bcryptjs: Encriptar
-//Create the method to Login use Post
+
+export async function loginUser(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        res.status(200).json({
+            message: 'Login successful',
+            token,
+            user: {
+                id: user._id,
+                email: user.email,
+            },
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+}
+
+export async function getUser(req, res) {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Users were not found', error });
+    }
+}
+
+export async function updateUser(req, res) {
+    const { id } = req.params;
+    const body = req.body;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, body, { new: true });
+        res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: 'User was not updated successfully', error });
+    }
+}
+
+export async function deleteUser(req, res) {
+    const { id } = req.params;
+    try {
+        const deletedUser = await User.findByIdAndDelete(id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User deleted successfully', user: deletedUser });
+    } catch (error) {
+        res.status(500).json({ message: 'User was not deleted successfully', error });
+    }
+}
--- models/server.js
+++ models/server.js
@@ -1,35 +1,43 @@
-import express from 'express'
-import 'dotenv/config'
-import dbConnection from '../database/config.js'
-import {getVehicle, postVehicle, putVehicle, deleteVehicle} from '../controllers/vehicleController.js'
-import {createUser} from '../controllers/userController.js'
+import express from 'express';
+import 'dotenv/config';
+import dbConnection from '../database/config.js';
+import { getVehicle, postVehicle, putVehicle, deleteVehicle } from '../controllers/vehicleController.js';
+import { createUser, getUser, updateUser, deleteUser, loginUser } from '../controllers/userController.js';
 
-export default class Server{
-    constructor(){
-        
-        this.app = express()
-        this.listen()
-        this.dbConnect()
-        this.pathVehicle = '/api/vehicle' //Link public API
-        this.route()
+export default class Server {
+    constructor() {
+        this.app = express();
+        this.listen();
+        this.dbConnect();
+        this.pathVehicle = '/api/vehicle'; // Link public API
+        this.pathUser = '/api/user'; // Link user API
+        this.route();
     }
 
-    listen(){//Method to listen the port
+    listen() { // Method to listen on the port
         this.app.listen(process.env.PORT, () => {
-            console.log(`Server is running in PORT ${process.env.PORT}`)
-        })
+            console.log(`Server is running on PORT ${process.env.PORT}`);
+        });
     }
 
-    async dbConnect(){ //Call method dbConnection to connect to Mongo
-        await dbConnection()
+    async dbConnect() { // Call method dbConnection to connect to Mongo
+        await dbConnection();
     }
 
-    route(){
-        this.app.use(express.json())//Convert data to json
-        this.app.get(this.pathVehicle, getVehicle)
-        this.app.post(this.pathVehicle, postVehicle)
-        this.app.put(this.pathVehicle, putVehicle)
-        this.app.delete(this.pathVehicle+('/:id'), deleteVehicle)
-        this.app.post('/user', createUser)
+    route() {
+        this.app.use(express.json()); // Convert data to json
+
+        // Vehicle routes
+        this.app.get(this.pathVehicle, getVehicle);
+        this.app.post(this.pathVehicle, postVehicle);
+        this.app.put(this.pathVehicle + '/:id', putVehicle); // Fixed route for PUT
+        this.app.delete(this.pathVehicle + '/:id', deleteVehicle); // Fixed route for DELETE
+
+        // User routes
+        this.app.post(`${this.pathUser}/create`, createUser); // Create user
+        this.app.post(`${this.pathUser}/login`, loginUser); // Login user
+        this.app.get(this.pathUser, getUser); // Get user (probably needs an ID or similar to be effective)
+        this.app.put(this.pathUser + '/:id', updateUser); // Update user
+        this.app.delete(this.pathUser + '/:id', deleteUser); // Delete user
     }
 }
